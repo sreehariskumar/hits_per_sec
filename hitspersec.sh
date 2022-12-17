2c935064c9ee8012ed41f0998074adcb319dae0e
SREEHARI <68052722+sreehariskumar@users.noreply.github.com>
2022-12-17 18:36:47 +0530

    Update hitspersec.sh

--- hitspersec.sh
+++ hitspersec.sh
@@ -1,4 +1,8 @@
-while true; do tail -n0 -f /var/log/apache2/access.log>/tmp/tmp.log & sleep 1; kill $! ; wc -l /tmp/tmp.log | cut -c-2; done 2>/dev/null ||
-while true; do tail -n0 -f /var/log/httpd/access.log>/tmp/tmp.log & sleep 1; kill $! ; wc -l /tmp/tmp.log | cut -c-2; done 2>/dev/null ||
-while true; do tail -n0 -f /var/log/nginx/access.log>/tmp/tmp.log & sleep 1; kill $! ; wc -l /tmp/tmp.log | cut -c-2; done 2>/dev/null ||
-while true; do tail -n0 -f /var/log/lighthttpd/access.log>/tmp/tmp.log & sleep 1; kill $! ; wc -l /tmp/tmp.log | cut -c-2; done 2>/dev/null
+#!/bin/bash
+while true; do 
+if  (pgrep apache > /dev/null); then
+tail -n0 -f /var/log/apache2/access.log>/tmp/tmp.log & sleep 1; kill $! ; wc -l /tmp/tmp.log | cut -c-2
+elif (pgrep nginx > /dev/null); then
+tail -n0 -f /var/log/nginx/access.log>/tmp/tmp.log & sleep 1; kill $! ; wc -l /tmp/tmp.log | cut -c-2
+fi
+done 2>/dev/null
